67f8548bc81ac817641e6ab3b9a0260e4dd5b826
Aiman03at <aiman03at@gmail.com>
2024-07-12 22:00:29 -0400

    Serialize form data and Submit a post request using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,54 +33,100 @@ const data = [
 ]
 
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  tweets.forEach(tweet => {
-    // calls createTweetElement for each tweet
-    const tweetElement = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').prepend(tweetElement);
-  });
-}
 
-const createTweetElement = function(tweet) {
-  // Your code for creating the tweet element
-  const timeAgo = (Date.now() - tweet.created_at) / (1000 * 60 * 60 * 24);
-  const $tweet = $(`
-    <article class="tweet">
-          
-          <!--Header for the tweet container-->
-          <header class="tweet-header">
-            <div>
-               <span><img src="${tweet.user.avatars}" alt="no image"> ${tweet.user.name}</span>
-               <span>${tweet.user.handle}</span>
-            </div>
-            
-          </header>
 
-          <div class="tweet-text">
-            <span> ${tweet.content.text}</span>
-            <hr>
-         </div>
-         
 
-          <!---Footer for the tweet container-->
-          <footer>
-            <div class="time-stamp">
-                <span>${Math.floor(timeAgo)} days ago</span>
-            </div>
-            <div class="icons">
-                <i class="fa-solid fa-flag"></i>
-                <i class="fa-solid fa-retweet"></i>
-                <i class="fa-solid fa-heart"></i>
-            </div>
-        </footer>
-    
-    </article>
-  `);
-  return $tweet;
-}
 $(document).ready(function() {
-  renderTweets(data);
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    tweets.forEach(tweet => {
+      // calls createTweetElement for each tweet
+      const tweetElement = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').prepend(tweetElement);
+    });
+  }
+
+  const createTweetElement = function(tweet) {
+    // Your code for creating the tweet element
+    const timeAgo = (Date.now() - tweet.created_at) / (1000 * 60 * 60 * 24);
+    const $tweet = $(`
+      <article class="tweet">
+            
+            <!--Header for the tweet container-->
+            <header class="tweet-header">
+              <div>
+                 <span><img src="${tweet.user.avatars}" alt="no image"> ${tweet.user.name}</span>
+                 <span>${tweet.user.handle}</span>
+              </div>
+              
+            </header>
+  
+            <div class="tweet-text">
+              <span> ${tweet.content.text}</span>
+              <hr>
+           </div>
+           
+  
+            <!---Footer for the tweet container-->
+            <footer>
+              <div class="time-stamp">
+                  <span>${Math.floor(timeAgo)} days ago</span>
+              </div>
+              <div class="icons">
+                  <i class="fa-solid fa-flag"></i>
+                  <i class="fa-solid fa-retweet"></i>
+                  <i class="fa-solid fa-heart"></i>
+              </div>
+          </footer>
+      
+      </article>
+    `);
+    return $tweet;
+  }
+
+  const loadTweets = function() {
+    $.ajax({
+      method : 'GET',
+      url :'/tweet',
+      success : (dataFromServer) =>{
+        console.log(dataFromServer);
+        renderTweets(dataFromServer);
+      }
+    })
+
+  }
+
+  loadTweets();
+
+  //Grab the new tweet from the form
+  const $form = $('#new-tweet-form');
+
+  //listen for submit event of form
+
+  $form.on('submit', (event)=>{
+    event.preventDefault();
+    
+    ///grab the form data
+    //create a url -encoded string for post to send
+    const formData = form.serialize();
+  });
+
+  //POST the form information to the server
+  $.ajax({
+    method:'POST',
+    url:'/tweets',
+    data: formData,
+    success:(response)=>{
+      console.log(response);
+      //reftch the data
+      loadTweets();
+
+    }
+  })
+  
 });
+
+
 
